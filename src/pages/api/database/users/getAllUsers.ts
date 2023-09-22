65287b68b1a93c0bf84a127ff2f5ff2104ffa09b
Carlos Rodriguez <crodriguezyt10@gmail.com>
2023-09-22 12:03:36 -0700

    Entered prisma connection files, still need to add client files.

---
+++ src/pages/api/database/users/getAllUsers.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/lib/database/prisma/client";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    try {
+
+    } catch (error) {
+        
+    }
+}
