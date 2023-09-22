bc1222cd0f9695cdc04a36aa1835871e4e58d5dc
Carlos Rodriguez <crodriguezyt10@gmail.com>
2023-09-22 09:27:34 -0700

    Fixed the CSS for darkmode and lightmode for the navbar.

--- src/components/BudgetOverview/BudgetOverview.tsx
+++ src/components/BudgetOverview/BudgetOverview.tsx
@@ -3,19 +3,29 @@ import { useUser } from '@auth0/nextjs-auth0/client';
 import { useColorMode } from '@chakra-ui/react'; // Import useColorMode
 
 const BudgetOverview = () => {
-  const { user, error, isLoading } = useUser();
-  const { colorMode } = useColorMode(); // Get the current color mode
-  const isDarkMode = colorMode === 'dark'; // Check if it's dark mode
-  const customColorModeClass = isDarkMode ? 'dark' : 'light'; // Determine the appropriate class
+    const { user, error, isLoading } = useUser();
+
+    const { colorMode } = useColorMode(); // Get the current color mode from useColorMode
+    const isDarkMode = colorMode === 'dark'; // Check if it's dark mode
 
 
-  return (
-    <div className={`${customColorModeClass}`} >
-      <div>{user?.name}</div>
-      {user?.nickname}
-      {customColorModeClass}
-    </div>
-  );
+
+    if (user){
+        return (
+            <div className={`${isDarkMode ? 'dark darkModeShadow' : 'light lightModeShadow'} m-4 p-4 rounded-lg`} >
+                <div>{user?.name}</div>
+                {user?.nickname}
+            </div>
+        );
+    }
+    if (!user){
+        return (
+            <div>
+                nope
+            </div>
+        )
+    }
+
 };
 
 export default BudgetOverview;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import Link from 'next/link';
+import { useColorMode } from '@chakra-ui/react'; // Import useColorMode
+
 
 function Navbar() {
   const { user, error, isLoading } = useUser();
 
+  const { colorMode } = useColorMode(); // Get the current color mode from useColorMode
+  const isDarkMode = colorMode === 'dark'; // Check if it's dark mode
+  const customColorModeClass = isDarkMode ? 'dark' : 'light'; // Determine the appropriate class
 
   if (isLoading) {
     return <div></div>;
   }
 
   return (
-    <nav className="bg-gradient-to-b from-blue-500 to-blue-900 text-white p-2 h-12 flex justify-between items-center">
+    <nav className={`${customColorModeClass} p-2 h-12 flex justify-between items-center`}>
       <ul className="flex list-none m-0 p-0">
         <li className="ml-6">
           <Link href="/">Home</Link>
@@ -28,7 +33,7 @@ function Navbar() {
         ) : (
           <div className="flex items-center">
             <li className="ml-6">
-              <Link href="/api/auth/login" className="text-white border border-white border-opacity-50 rounded-md px-2 py-1 font-bold hover:border-opacity-100 transition">
+              <Link href="/api/auth/login" className=" border border-2 rounded-md px-2 py-1 font-bold ">
                 Login
               </Link>
             </li>
@@ -44,7 +49,7 @@ function Navbar() {
               className="w-10 h-10 rounded-full mr-2"
             />
           )}
-          <Link href="/api/auth/logout" className="text-white border border-white border-opacity-50 rounded-md px-2 py-1 font-bold hover:border-opacity-100 transition">
+          <Link href="/api/auth/logout" className="border border-2 rounded-md px-2 py-1 font-bold">
             Logout
           </Link>
         </div>
